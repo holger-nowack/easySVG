20c9a74ca444fdb6c38cb308796c970f3e2e819f
holger-nowack <holger-nowack@users.noreply.github.com>
2015-04-30 23:04:58 +0200

    add write function

--- src/easySVG/core.py
+++ src/easySVG/core.py
@@ -1,12 +1,14 @@
 from xml.etree.ElementTree import Element, ElementTree, tostring
 import collections
+from io import BytesIO as SVGIO
+import xml.dom.minidom
 
 _precision = 9
 
 def set_precision(n=3):
     global _precision
     _precision = n
- 
+
 def parse_attribs(attrib):
     att = {}
     for key in attrib:
@@ -26,11 +28,21 @@ class SVGElement(Element):
     def __init__(self, tag, attrib={}, **extra):
         att = parse_attribs(attrib)
         super(self.__class__, self).__init__(tag, att, **extra)
-        
-        
+
+
 def SVGSubElement(parent, tag, attrib={}, **extra):
     att = parse_attribs(attrib)
     svge = SVGElement(tag, att, **extra)
     parent.append(svge)
     return svge
-    
+
+def write(elem, filename, encoding='utf-8', indent='    '):
+    tree = ElementTree(elem)
+    io = SVGIO()
+    tree.write(io, encoding=encoding, xml_declaration=True, method='xml')
+
+    dom = xml.dom.minidom.parseString(io.getvalue())
+    pretty_str = dom.toprettyxml(indent=indent,
+                                 encoding=encoding).decode(encoding)
+    with open(filename, 'w') as f:
+        f.write((pretty_str))
